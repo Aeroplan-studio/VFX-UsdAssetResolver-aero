81bc14665ad5c87b83a8fbe71df9a8c922914f1e
achernysh-dev <andrew.a.chernysh@gmail.com>
2025-07-21 15:08:11 +0300

    bat file for houdini 19.5

---
+++ build_houdini_19_5.bat
@@ -0,0 +1,15 @@
+REM Clear current session log 
+cls
+REM Source environment (Uncomment lines starting with "set" if you current env does not have these defined.)
+set HFS=C:\Program Files\Side Effects Software\Houdini 19.5.435
+REM Define Resolver > Has to be one of 'fileResolver'/'pythonResolver'/'cachedResolver'/'httpResolver'
+set AR_RESOLVER_NAME=cachedResolver
+REM Define App
+set AR_DCC_NAME=HOUDINI
+REM Clear existing build data and invoke cmake
+rmdir /S /Q build
+rmdir /S /Q dist
+REM Make sure to match the correct VS version the DCC was built with
+cmake . -B build -G "Visual Studio 17 2022" -A x64 -T v143
+cmake --build build  --clean-first --config Release
+cmake --install build
