da51b50150138a2d54f85d1a1435519d5adfbb8b
achernysh-dev <andrew.a.chernysh@gmail.com>
2025-08-11 18:35:43 +0300

    add compile bash for houdini 19.5

---
+++ build_houdini_19_5.sh
@@ -0,0 +1,16 @@
+# Clear current session log 
+clear
+# Source environment (Uncomment lines starting with "export" if you current env does not have these defined.)
+export HFS=$SMESHARIKI_ROOT/sidefx/hfs19.5
+# Define Resolver > Has to be one of 'fileResolver'/'pythonResolver'/'cachedResolver'/'httpResolver'
+export AR_RESOLVER_NAME=cachedResolver
+# Define App
+export AR_DCC_NAME=HOUDINI
+# Clear existing build data and invoke cmake
+# rm -R build
+# rm -R dist
+set -e # Exit on error
+cmake . -B build
+cmake --build build --clean-first              # make clean all
+cmake --install build                          # make install
+# ctest -VV --test-dir build
