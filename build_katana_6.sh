f1de7705ad411cb9a7f275234aecc8d3e0e09cbf
achernysh-dev <andrew.a.chernysh@gmail.com>
2025-08-14 14:58:24 +0300

    updated Katana6 build

---
+++ build_katana_6.sh
@@ -0,0 +1,19 @@
+# Clear current session log 
+clear
+# Source environment (Uncomment lines starting with "export" if you current env does not have these defined.)
+# export HFS=$SMESHARIKI_ROOT/sidefx/hfs19.5
+# Define Resolver > Has to be one of 'fileResolver'/'pythonResolver'/'cachedResolver'/'httpResolver'
+export AR_RESOLVER_NAME=cachedResolver
+# Define App
+export AR_DCC_NAME=KATANA
+export KATANA_ROOT=/server/sm/sm_tools/katana/distrib/Katana6.0v1
+export PYTHON_ROOT=/server/sm/sm_tools/katana/distrib/Katana6.0v1/bin/python3.9
+
+# Clear existing build data and invoke cmake
+# rm -R build
+# rm -R dist
+set -e # Exit on error
+cmake . -B build
+cmake --build build --clean-first              # make clean all
+cmake --install build                          # make install
+# ctest -VV --test-dir build
