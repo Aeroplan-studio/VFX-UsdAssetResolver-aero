6f751dd5670257c1ae1d3fc1b74d939acd00a6ad
achernysh-dev <andrew.a.chernysh@gmail.com>
2025-10-01 15:06:59 +0300

    test change

--- docs.sh
+++ docs.sh
@@ -2,5 +2,5 @@
 export REPO_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
 # Clean existing build
 rm -R ${REPO_ROOT}/docs/book
-# Build book
+# Build books
 mdbook serve --open ${REPO_ROOT}/docs
